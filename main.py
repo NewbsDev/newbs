334211778c394d8e597e9016ba27c0e7a3016fc7
james-caldwell1981 <james.caldwell.1981@gmail.com>
2022-06-23 16:42:03 -0400

    removed async from routes

--- main.py
+++ main.py
@@ -7,25 +7,25 @@ import white_stars as ws
 app = Flask(__name__)
 
 @app.route('/')
-async def root():
+def root():
 
     return {'message': 'Hey! We\'re trying to work here!'}
 
 
 @app.route('/welcome')
-async def welcome(rndm_place: str = 'sideshow') -> dict:
+def welcome(rndm_place: str = 'sideshow') -> dict:
 
     return {'message': f'Welcome to the {rndm_place}.'}
 
 
 @app.route('/red_stars')
-async def red_stars() -> dict:
+def red_stars() -> dict:
 
     return {'message': 'Don\'t forget your miners.'}
 
 
 @app.route('/white_stars')
-async def white_stars() -> dict:
+def white_stars() -> dict:
 
     return {'message': 'Get ready to rumble!'}
 
